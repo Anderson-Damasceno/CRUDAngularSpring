a90b8602dba888fe45240853d2d8aa360773bcc3
Anderson Damasceno <willk@atlantico04416.virgo.atlantico.net.br>
2022-09-23 18:59:17 -0300

    new repository

---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
+
+import { Course } from '../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpCliente: HttpClient) { }
+
+  list() {
+    return this.httpCliente.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(3000),
+      tap(courses => console.log(courses))
+    );
+  }
+}
